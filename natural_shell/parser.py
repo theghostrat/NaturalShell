bb1c3e4dcec9a974a6be9e5b199b6d37e71e5bdd
fellowtraveler <f3llowtraveler@gmail.com>
2024-05-12 01:19:10 -0500

    fixed so it correctly finds config.yaml based on NSHELL_CONFIG env var

--- natural_shell/parser.py
+++ natural_shell/parser.py
@@ -2,6 +2,6 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
 
 class Command(BaseModel):
-    command: str = Field(None, description="The real command based on the input")
+    command: str = Field(None, description="The intended CLI command, based on the input")
 
 parser = JsonOutputParser(pydantic_object=Command)
